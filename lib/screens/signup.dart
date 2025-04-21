49fbd65935aa3da5c74db88bd33ef2179e41ab5d
Shreya <write2sdp@hotmail.com>
2025-04-22 00:32:59 +0530

    removed unused import

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,7 +1,6 @@
 import 'package:demo_app_v1_1/bloc/register/register_bloc.dart';
 import 'package:demo_app_v1_1/bloc/register/register_state.dart';
 import 'package:demo_app_v1_1/routes.dart';
-import 'package:demo_app_v1_1/screens/home.dart';
 import 'package:demo_app_v1_1/widgets/registration_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
